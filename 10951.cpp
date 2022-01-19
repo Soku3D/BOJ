b00958473ed7e021ab17bdf8f2102bc6a521c7f9
sonny-99 <83201492+Sontacs@users.noreply.github.com>
2022-01-20 00:06:37 +0900

    2022-01-20

---
+++ 10951.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(false);
+	int a, b;
+	while (cin >> a && cin>>b)
+	{
+		cout << a + b <<'\n';
+	}
+}
---
+++ 11718.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(false);
+	string a;
+	while (getline(cin,a))
+	{
+		cout << a;
+	}
+}
